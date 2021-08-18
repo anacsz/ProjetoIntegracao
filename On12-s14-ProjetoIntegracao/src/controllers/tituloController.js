058d127004870d3bacddab499ea3722de1931f27
Ana Claudia <anac.almeidasouza@gmail.com>
2021-08-18 20:49:33 -0300

    second commit

--- On12-s14-ProjetoIntegracao/src/controllers/tituloController.js
+++ On12-s14-ProjetoIntegracao/src/controllers/tituloController.js
@@ -7,6 +7,12 @@ const getAllPixar = async (req, res) => {
   res.json(titulosFiltrados)
 }
 
+const getAllMarvel = async (req, res) => {
+  const titulos = await Titulo.find().populate('estudio')
+  const titulosFiltrados = titulos.filter(titulo => titulo.estudio.nome == "Marvel")
+  res.json(titulosFiltrados)
+}
+
 const getAll = async (req, res) => {
   const titulos = await Titulo.find().populate('estudio')
   res.status(200).json(titulos)
@@ -21,17 +27,59 @@ const createTitle = async (req, res) => {
     estudio: req.body.estudio,
     criadoEm: req.body.criadoEm
   })
-  //TODO : criar validação se filme já existe
-  try {
-    const novoTitulo = await titulo.save()
-    res.status(201).json(novoTitulo)
-  } catch (err) {
-    res.status(400).json({ message: err.message})
+  const tituloJaExiste = await Titulo.findOne({nome: req.body.nome})
+ if (tituloJaExiste) {
+   return res.status(409).json({error: 'Estudio ja consta cadastrado.'})
+ }
+  try{
+      const novoTitulo = await titulo.save()
+      res.status(201).json(novoTitulo)
+  }catch (err){
+      res.status(400).json({message: err.message})
   }
 }
+
+const updateOne = async (req, res) => {
+  try {
+    
+   const titulo = await Titulo.findById(req.params.id)
+   
+   if (titulo == null){
+     return res.status(404).json({message: "Título não localizado"})
+   }
+   
+   if (req.body.nome != null){
+     titulo.nome = req.body.nome
+   }
+   const tituloAtualizado = await titulo.save()
+   
+   res.status(200).json(tituloAtualizado)
+   
+  } catch (err){
+   res.status(500).json({ message: err.message})
+  }
+}
+
+const deleteById = async (req, res) => {
+  const titulo = await Titulo.findById(req.params.id)
+      if (titulo == null){
+          return res.status(404).json({message: "Título não localizado."})
+  }
+  try{
+        await titulo.remove()
+          res.status(200).json({message:"Título excluido com sucesso."})
+      } catch (err){
+          res.status(500).json({message: err.message})
+    }
+}
+
 
 module.exports = {
   getAll,
   createTitle,
-  getAllPixar
+  getAllPixar,
+  getAllMarvel,
+  createTitle,
+  updateOne,
+  deleteById
 }
--- On12-s14-ProjetoIntegracao/src/routes/titulos.routes.js
+++ On12-s14-ProjetoIntegracao/src/routes/titulos.routes.js
@@ -5,15 +5,18 @@ const controller = require('../controllers/tituloController')
 router.get('/pixar', controller.getAllPixar)
 
 
-router.get('/', controller.getAll)
+router.get('/marvel', controller.getAllMarvel)
 
 
 router.post('/', controller.createTitle)
 
 //listar um titulo/get/findById
+router.get('/', controller.getAll)
 
 //atualizar uma informacao especifica num titulo/patch/findById/save
+router.patch('/:id', controller.updateOne)
 
 //deletar um titulo/delete/findById/remove
+router.delete('/:id', controller.deleteById)
 
 module.exports = router
